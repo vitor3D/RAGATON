757195e08fb8672886c2b60f0f879457e94adff8
vitor3Dev <79985842+vitor3D@users.noreply.github.com>
2023-05-04 00:07:20 -0300

    Add files via upload

--- scr/js.js
+++ scr/js.js
@@ -1,19 +1,1 @@
-window.onload = function() {
-    // selecionar o botão do menu
-    var menuButton = document.querySelector('.menu-icon');
 
-    // adicionar um evento de clique ao botão do menu
-    menuButton.addEventListener('click', function() {
-      // selecionar o dropdown
-      var dropdown = document.querySelector('.dropdown');
-
-      // se o dropdown está visível, ocultá-lo
-      if (dropdown.classList.contains('show')) {
-        dropdown.classList.remove('show');
-      }
-      // caso contrário, mostrá-lo
-      else {
-        dropdown.classList.add('show');
-      }
-    });
-  };
